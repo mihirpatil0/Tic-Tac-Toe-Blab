ee7364037f50385dcfce3c0feeb67a08f5e59652
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-04 09:32:03 +0530

    Refactored: Added Comments where ever required

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,23 +2,29 @@ package tictactoe;
 
 public class TicTacToeGame 
 {	
-	public char[][] tttBoard = new char[3][3];
+	static char[][] tttBoard = new char[3][3];
 	
-	void createBoard()
+	/**
+	 * {@summary}
+	 * Created matrix board.
+	 * 0th index is ignored.
+	 * assigning empty space to remaining blocks.
+	 */
+	public static void createBoard()
 	{
 		for(int i=0; i<tttBoard.length; i++)
 		{
-			for (int j=0; j<tttBoard.length; j++)
+			for (int j=1; j<tttBoard.length; j++)
 			{	
-				tttBoard[i][j] = '-';
+				tttBoard[i][j] = ' '; 
 			}
 		}
 	}
 	
 	public static void main(String[] args) 
-	{
-		TicTacToeGame tttGame = new TicTacToeGame();
-		tttGame.createBoard();
+	{	
+		//calling the createBoard method
+		createBoard();
 	}
 }
 
